eec9427b738018da0897a747559c6e700de89e2c
Elmosius <2272008@maranatha.ac.id>
2024-11-04 22:05:45 +0700

    updated icon.js

--- close-tab/components/icon.js
+++ close-tab/components/icon.js
@@ -1,41 +1,34 @@
-import { TransformasiMatriks } from "./transformasi.js";
-
 export class IconClose {
-  static draw(canvas, posX = 0, posY = 0, size = 50, border = true, padding = 15, color = 0, borderColor = 0) {
-    const tm = TransformasiMatriks;
-
+  static draw(canvas, posX = 0, posY = 0, size = 50, border = true, padding = 15, color = { r: 0, g: 0, b: 0 }, borderColor = { r: 0, g: 0, b: 0 }) {
     const halfSize = (size - padding * 2) / 2;
 
     const points = [
-      { x: -halfSize, y: -halfSize },
-      { x: halfSize, y: halfSize },
-      { x: -halfSize, y: halfSize },
-      { x: halfSize, y: -halfSize },
+      { x: posX - halfSize, y: posY - halfSize },
+      { x: posX + halfSize, y: posY + halfSize },
+      { x: posX - halfSize, y: posY + halfSize },
+      { x: posX + halfSize, y: posY - halfSize },
     ];
 
-    const translationMatrix = tm.createTranslasi(posX, posY);
-    const transformedPoints = tm.transformasiArray(points, translationMatrix);
-
-    canvas.garis(transformedPoints[0].x, transformedPoints[0].y, transformedPoints[1].x, transformedPoints[1].y, color);
-    canvas.garis(transformedPoints[2].x, transformedPoints[2].y, transformedPoints[3].x, transformedPoints[3].y, color);
+    canvas.garis(points[0].x, points[0].y, points[1].x, points[1].y, color);
+    canvas.garis(points[2].x, points[2].y, points[3].x, points[3].y, color);
 
     if (border) {
       const boxHalfSize = halfSize + padding;
       const boxPoints = [
-        { x: -boxHalfSize, y: -boxHalfSize },
-        { x: boxHalfSize, y: -boxHalfSize },
-        { x: boxHalfSize, y: boxHalfSize },
-        { x: -boxHalfSize, y: boxHalfSize },
-        { x: -boxHalfSize, y: -boxHalfSize },
+        { x: posX - boxHalfSize, y: posY - boxHalfSize },
+        { x: posX + boxHalfSize, y: posY - boxHalfSize },
+        { x: posX + boxHalfSize, y: posY + boxHalfSize },
+        { x: posX - boxHalfSize, y: posY + boxHalfSize },
+        { x: posX - boxHalfSize, y: posY - boxHalfSize },
       ];
-      const transformedBoxPoints = tm.transformasiArray(boxPoints, translationMatrix);
 
-      for (let i = 0; i < transformedBoxPoints.length - 1; i++) {
-        const p1 = transformedBoxPoints[i];
-        const p2 = transformedBoxPoints[i + 1];
+      for (let i = 0; i < boxPoints.length - 1; i++) {
+        const p1 = boxPoints[i];
+        const p2 = boxPoints[i + 1];
         canvas.garis(p1.x, p1.y, p2.x, p2.y, borderColor);
       }
-      canvas.floodFillStack(posX+1, posY=1, { r: 0, g: 0, b: 0 }, borderColor);
+
+      canvas.floodFillStack(posX - boxHalfSize + 1, posY - boxHalfSize + 1, { r: 0, g: 0, b: 0 }, borderColor);
     }
 
     canvas.draw();
