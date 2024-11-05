9b252a069c669770e898dda68078db708eb6ce86
Elmosius <2272008@maranatha.ac.id>
2024-11-05 23:30:28 +0700

    created tali ketapel

--- close-tab/components/ketapel.js
+++ close-tab/components/ketapel.js
@@ -1,42 +1,70 @@
 import { TransformasiMatriks } from "./transformasi.js";
 
 export class Ketapel {
-  static draw(canvas, posX = 0, posY = 0, scale = 1, color = { r: 150, g: 75, b: 0 }) {
-    const points = [
-      // Pegangan bawah
-      { x: 0, y: 30 },
-      // Titik tengah 
-      { x: 0, y: 0 },
-      // Ujung kiri
-      { x: -20, y: -50 },
-      // Kembali ke tengah
-      { x: 0, y: 0 },
-      // Ujung kanan
-      { x: 20, y: -50 },
-      // Kembali ke tengah
-      { x: 0, y: 0 },
+  static draw(canvas, posX = 0, posY = 0, scale = 1, color = { r: 150, g: 75, b: 0 }, angleLeft = Math.PI / 20, angleRight = -Math.PI / 10, dalamTali = 200) {
+    const center = { x: posX, y: posY };
+
+    const handlePoints = [
+      { x: 0, y: 30 }, // Pegangan bawah
+      { x: 0, y: 0 }, // Titik tengah
     ];
 
+    const leftPoints = [
+      { x: 0, y: 0 }, // Titik tengah
+      { x: -20, y: -50 }, // Ujung kiri
+    ];
+
+    const rightPoints = [
+      { x: 0, y: 0 }, // Titik tengah
+      { x: 20, y: -50 }, // Ujung kanan
+    ];
+
+    // buat pegangan
     const translationMatrix = TransformasiMatriks.createTranslasi(posX, posY);
     const scaleMatrix = TransformasiMatriks.createSkala(scale, scale);
     const transformMatrix = TransformasiMatriks.mutiplyMatrix(translationMatrix, scaleMatrix);
-    const transformedPoints = TransformasiMatriks.transformasiArray(points, transformMatrix);
+    const transformedHandlePoints = TransformasiMatriks.transformasiArray(handlePoints, transformMatrix);
 
-    for (let i = 0; i < transformedPoints.length - 1; i++) {
-      const p1 = transformedPoints[i];
-      const p2 = transformedPoints[i + 1];
-      const dx = p2.x - p1.x;
-      const dy = p2.y - p1.y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      const step = 1;
+    // batang kiri dan kanan
+    const leftRotationMatrix = TransformasiMatriks.rotasiFixPoint(center.x, center.y, angleLeft);
+    const leftTransformMatrix = TransformasiMatriks.mutiplyMatrix(leftRotationMatrix, transformMatrix);
+    const transformedLeftPoints = TransformasiMatriks.transformasiArray(leftPoints, leftTransformMatrix);
 
-      for (let j = 0; j < distance; j += step) {
-        const x = p1.x + (dx / distance) * j;
-        const y = p1.y + (dy / distance) * j;
-        canvas.titik(Math.round(x), Math.round(y), color);
-      }
-    }
+    const rightRotationMatrix = TransformasiMatriks.rotasiFixPoint(center.x, center.y, angleRight);
+    const rightTransformMatrix = TransformasiMatriks.mutiplyMatrix(rightRotationMatrix, transformMatrix);
+    const transformedRightPoints = TransformasiMatriks.transformasiArray(rightPoints, rightTransformMatrix);
+
+    this.drawLineWithSteps(canvas, transformedHandlePoints[0], transformedHandlePoints[1], color);
+    this.drawLineWithSteps(canvas, transformedLeftPoints[0], transformedLeftPoints[1], color);
+    this.drawLineWithSteps(canvas, transformedRightPoints[0], transformedRightPoints[1], color);
+
+    // gambar tali
+    this.drawFlexibleString(canvas, transformedLeftPoints[1], transformedRightPoints[1], color, dalamTali);
 
     canvas.draw();
+  }
+
+  static drawLineWithSteps(canvas, p1, p2, color) {
+    const dx = p2.x - p1.x;
+    const dy = p2.y - p1.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    const step = 1;
+
+    for (let j = 0; j < distance; j += step) {
+      const x = p1.x + (dx / distance) * j;
+      const y = p1.y + (dy / distance) * j;
+      canvas.titik(Math.round(x), Math.round(y), color);
+    }
+  }
+
+  static drawFlexibleString(canvas, leftPoint, rightPoint, color, curveDepth = 20) {
+    const midX = (leftPoint.x + rightPoint.x) / 2;
+    const midY = (leftPoint.y + rightPoint.y) / 2 - curveDepth; // Tambahkan `curveDepth` untuk ketinggian lengkungan
+
+    for (let t = 0; t <= 1; t += 0.01) {
+      const x = (1 - t) * (1 - t) * leftPoint.x + 2 * (1 - t) * t * midX + t * t * rightPoint.x;
+      const y = (1 - t) * (1 - t) * leftPoint.y + 2 * (1 - t) * t * midY + t * t * rightPoint.y;
+      canvas.titik(Math.round(x), Math.round(y), color);
+    }
   }
 }
--- close-tab/components/tab.js
+++ close-tab/components/tab.js
@@ -1,4 +1,5 @@
 import { IconClose } from "../components/icon.js";
+import { Ketapel } from "./ketapel.js";
 
 export class WebTab {
   constructor(canvas, width, height, headerHeight = 32, headerColor = { r: 125, g: 156, b: 166 }, contentColor = { r: 245, g: 245, b: 245 }) {
@@ -12,7 +13,7 @@ export class WebTab {
     this.iconSize = 30;
     this.iconPadding = 10;
     this.iconX = width - this.iconSize + 14;
-    this.iconY = 15;
+    this.iconY = 14;
 
     this.contentPoints = [
       { x: 0, y: this.headerHeight },
@@ -24,7 +25,8 @@ export class WebTab {
     this.isHovered = false;
     this.imageLoaded = false;
     this.isCleared = false;
-    this.initHoverEffect();
+    this.iconHovered();
+    this.iconCliked();
   }
 
   drawTab() {
@@ -44,9 +46,49 @@ export class WebTab {
 
     IconClose.draw(this.canvas, this.iconX, this.iconY, this.iconSize, false, this.iconPadding, 0, 0);
     this.canvas.draw();
+
+    this.gambar();
   }
 
-  initHoverEffect() {
+  gambar() {
+    const img = new Image();
+    img.src = "./img/gbr_yt.png";
+
+    img.onload = () => {
+      console.log("gambar muncul");
+      const context = this.canvas.ctx;
+      const imageX = 10;
+      const imageY = this.headerHeight + 10;
+      const imageWidth = this.width - 20;
+      const imageHeight = this.height - this.headerHeight - 20;
+
+      context.drawImage(img, imageX, imageY, imageWidth, imageHeight);
+      this.imageLoaded = true;
+    };
+  }
+
+  iconCliked() {
+    this.canvas.c_handler.addEventListener("click", (e) => {
+      const mouseX = e.offsetX;
+      const mouseY = e.offsetY;
+
+      const iconBorderX = this.iconX - this.iconPadding;
+      const iconBorderY = this.iconY - this.iconPadding;
+      const iconBorderSize = this.iconSize + this.iconPadding * 2;
+
+      if (mouseX >= iconBorderX && mouseX <= iconBorderX + iconBorderSize && mouseY >= iconBorderY && mouseY <= iconBorderY + iconBorderSize) {
+        this.isCleared = true;
+        this.canvas.clear();
+        this.canvas.c_handler.style.cursor = "default";
+
+        const ketapelPosX = this.width / 2;
+        const ketapelPosY = this.height - 30;
+        Ketapel.draw(this.canvas, ketapelPosX, ketapelPosY);
+      }
+    });
+  }
+
+  iconHovered() {
     this.canvas.c_handler.addEventListener("mousemove", (e) => {
       if (this.isCleared) return;
 
@@ -60,6 +102,7 @@ export class WebTab {
       const isInsideIcon = mouseX >= iconBorderX && mouseX <= iconBorderX + iconBorderSize && mouseY >= iconBorderY && mouseY <= iconBorderY + iconBorderSize;
 
       if (isInsideIcon && !this.isHovered) {
+        console.info("kena");
         this.isHovered = true;
         this.canvas.c_handler.style.cursor = "pointer";
         this.canvas.clear();
@@ -74,28 +117,14 @@ export class WebTab {
         this.canvas.floodFillStack(5, 5, { r: 0, g: 0, b: 0 }, this.headerColor);
         this.canvas.polygon(this.contentPoints, this.contentColor);
         this.canvas.floodFillStack(5, this.headerHeight + 5, { r: 0, g: 0, b: 0 }, this.contentColor);
-
         IconClose.draw(this.canvas, this.iconX, this.iconY, this.iconSize, true, this.iconPadding, { r: 255, g: 255, b: 255 }, { r: 255 });
+        this.drawTab();
         this.canvas.draw();
       } else if (!isInsideIcon && this.isHovered) {
         this.isHovered = false;
         this.canvas.c_handler.style.cursor = "default";
         this.canvas.clear();
         this.drawTab();
-      }
-    });
-
-    this.canvas.c_handler.addEventListener("click", (e) => {
-      const mouseX = e.offsetX;
-      const mouseY = e.offsetY;
-
-      const iconBorderX = this.iconX - this.iconPadding;
-      const iconBorderY = this.iconY - this.iconPadding;
-      const iconBorderSize = this.iconSize + this.iconPadding * 2;
-
-      if (mouseX >= iconBorderX && mouseX <= iconBorderX + iconBorderSize && mouseY >= iconBorderY && mouseY <= iconBorderY + iconBorderSize) {
-        this.isCleared = true;
-        this.canvas.clear();
       }
     });
   }
